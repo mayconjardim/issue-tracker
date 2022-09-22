b8926985fc156414ecd625eb12f96ed7093cfd61
Maycon Jardim <mayconsmill@gmail.com>
2022-09-22 20:02:40 -0300

    Issue report validation

--- src/app/issue-report/issue-report.component.ts
+++ src/app/issue-report/issue-report.component.ts
@@ -1,6 +1,6 @@
 import { IssuesService } from './../issues.service';
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-issue-report',
@@ -19,14 +19,19 @@ export class IssueReportComponent implements OnInit {
 
   ngOnInit(): void {
     this.issueForm = this.builder.group({
-      title: [''],
-      description: [''],
-      priority: [''],
-      type: [''],
+      title: ['', Validators.required],
+      description: ['', Validators.required],
+      priority: ['', Validators.required],
+      type: ['', Validators.required],
     });
   }
 
   addIssue() {
+    if (this.issueForm && this.issueForm.invalid) {
+      this.issueForm.markAllAsTouched();
+      return;
+    }
+
     this.issueService.createIssue(this.issueForm?.value);
     this.formClose.emit();
   }
