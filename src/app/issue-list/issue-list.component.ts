73104d7256287eb5996eab78772c00c7aff45770
Maycon Jardim <mayconsmill@gmail.com>
2022-09-22 19:13:36 -0300

    Issues list componente

--- src/app/issue-list/issue-list.component.ts
+++ src/app/issue-list/issue-list.component.ts
@@ -1,15 +1,22 @@
+import { IssuesService } from './../issues.service';
 import { Component, OnInit } from '@angular/core';
+import { Issue } from '../issue';
 
 @Component({
   selector: 'app-issue-list',
   templateUrl: './issue-list.component.html',
-  styleUrls: ['./issue-list.component.css']
+  styleUrls: ['./issue-list.component.css'],
 })
 export class IssueListComponent implements OnInit {
+  issues: Issue[] = [];
 
-  constructor() { }
+  constructor(private issuesService: IssuesService) {}
 
   ngOnInit(): void {
+    this.getIssues();
   }
 
+  private getIssues() {
+    this.issues = this.issuesService.getPendingIssues();
+  }
 }
