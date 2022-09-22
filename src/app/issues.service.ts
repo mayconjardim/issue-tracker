325e413e1d24185163b6140d37c44b7eee4fec13
Maycon Jardim <mayconsmill@gmail.com>
2022-09-22 19:07:45 -0300

    Component issue-list

--- src/app/issues.service.ts
+++ src/app/issues.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Issue } from './issue';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class IssuesService {
+  private issues: Issue[] = [];
 
-  constructor() { }
+  constructor() {}
+
+  getPendingIssues(): Issue[] {
+    return this.issues.filter((issue) => !issue.completed);
+  }
 }
